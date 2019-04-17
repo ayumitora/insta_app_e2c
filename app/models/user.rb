017b7c4672d0f2841bd5d921e6a2083de31c04c5
ayumi_arai <0araiayumi@gmail.com>
2019-04-17 16:50:36 +0900

    ログイン・ログアウト機能

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  before_validation { email.downcase! }
+  validates :user_name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :profile, length: { maximum: 500 }
+  has_secure_password
+end
