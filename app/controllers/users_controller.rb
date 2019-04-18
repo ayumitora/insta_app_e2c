aecfd5a94a4467cfd16e9be6cc2906a1046c98ca
ayumi_arai <0araiayumi@gmail.com>
2019-04-18 11:32:11 +0900

    画像アップロード機能、確認画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -6,18 +11,22 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to user_path(@user)
     else
       render 'new'
     end
   end
 
-  def show
+  def edit
     @user = User.find(params[:id])
-    if @user.save
-      redirect_to user_path(@user.id)
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path, notice: "ユーザー情報を編集しました。"
     else
-      render 'new'
+      render 'edit'
     end
   end
 
