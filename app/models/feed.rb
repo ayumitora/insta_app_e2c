4071185ead51176cacc90a4678d2822819891e07
ayumi_arai <0araiayumi@gmail.com>
2019-04-20 14:42:11 +0900

    日本語化機能、実装未確認

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,4 +1,5 @@
 class Feed < ApplicationRecord
+  validates :content, presence: true, length: {maximum: 500}
   mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :favorites, dependent: :destroy
