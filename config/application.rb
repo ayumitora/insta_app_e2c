5e8baa1e6ecbc2ea368130b53d207b9439e24da9
ayumi_arai <0araiayumi@gmail.com>
2019-04-20 16:18:45 +0900

    日本語化カスタム完了

--- config/application.rb
+++ config/application.rb
@@ -32,8 +32,6 @@ module InstaApp
     config.generators do |g|
       g.assets false
       g.helper false
-      config.i18n.default_locale = :ja
-      config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     end
   end
 end
