68b546a60fb504f98e21dc3c1677507a6d73020a
ayumi_arai <0araiayumi@gmail.com>
2019-04-27 10:22:12 +0900

    users/showへのリダイレクト確認

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # redirect_to action: 'show', id: @user.id
       redirect_to user_path(@user.id)
     else
       render 'new'
